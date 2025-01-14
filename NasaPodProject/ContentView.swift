bc3b5a3e8aa2eb327d2633ba3bdff3a1cbdbe8d3
emma <emmamathar@gmail.com>
2025-01-14 00:48:52 +0000

    Initial Commit

---
+++ NasaPodProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NasaPodProject
+//
+//  Created by Emmanuel Mathar on 14/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NasaPodProject/NasaPodProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaPodProjectApp.swift
+//  NasaPodProject
+//
+//  Created by Emmanuel Mathar on 14/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct NasaPodProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
